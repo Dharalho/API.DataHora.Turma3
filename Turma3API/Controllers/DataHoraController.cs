3173c4809429b80d0423f8bb9cb1eca8e359da7b
João Pedro <medeirosjpro@hotmail.com>
2021-07-23 20:11:06 -0300

    Add project files.

---
+++ Turma3API/Controllers/DataHoraController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+
+namespace Turma3API.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataHoraController : ControllerBase {
+
+        static DateTime dataHora = DateTime.Now;
+        static DateTime diaAula = new DateTime(2021, 7, 24, 8, 0, 0);
+
+        TimeSpan proxAula = diaAula.Subtract(dataHora);
+
+        [HttpGet]
+        public IActionResult Get() {
+            
+            return BadRequest("Agora: " + dataHora + " e Faltam " + proxAula.ToString("") + " para proxima aula" );
+        }
+
+    }
+}
